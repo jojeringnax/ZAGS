1fe0cfaf233c43adaa54add748f0d3bcd2731344
jojeringnax <hollow718@gmail.com>
2018-12-01 19:43:22 +0300

    Stacker

--- models/events/Payment.php
+++ models/events/Payment.php
@@ -36,18 +36,73 @@ class Payment extends Events
 
     /**
      * @param $deviceId
-     * @return int|mixed
+     * @return array
+     * @throws Exception
      */
-    public static function getStackerForDevice($deviceId)
+    public static function getStackerForDevices($deviceId)
     {
-        $result = 0;
-        $lastEncashment = Encashment::getLastEncashmentForDevice($deviceId);
-        $time = $lastEncashment === null ? '' : $lastEncashment->time;
-        $payments = self::find(null, false, true)->andWhere(['device_id' => $deviceId])->andWhere(['between', 'time', $time, date('Y-m-d H:i:s')])->all();
-        foreach ($payments as $payment) {
-            $result += $payment->data;
+        if (empty($deviceId) || $deviceId === null) {
+            throw new Exception('Need set variable');
         }
-        return $result;
+        $payments = self::find()->andWhere(['device_id' => $deviceId])->all();
+        if(is_array($deviceId)) {
+            if ($payments === null) {
+                foreach ($deviceId as $id) {
+                    $resultStacker[$id] = 0;
+                    $resultProfit[$id] = 0;
+                }
+                return array(
+                    'profit' => $resultProfit,
+                    'stacker' => $resultStacker
+                );
+            }
+            foreach ($deviceId as $id) {
+                $resultProfit[$id] = 0;
+                $resultStacker[$id] = 0;
+                $lastEncashment = Encashment::getLastEncashmentForDevice($id);
+                if ($lastEncashment === null) {
+                    foreach ($payments as $payment) {
+                        if ($payment->device_id === $id) {
+                            if ($payment->name === 'Money')
+                                $resultStacker[$id] += $payment->data;
+                            $resultProfit[$id] += $payment->data;
+                        }
+                    }
+                    continue;
+                }
+                $time = $lastEncashment->time;
+                foreach ($payments as $payment) {
+                    if (strtotime($payment->time) <= strtotime(date('Y-m-d')) && strtotime($payment->time) >= strtotime(date('Y-m-1'))) {
+                        $resultProfit[$id] += $payment->data;
+                    }
+                    if (strtotime($payment->time) > strtotime($time) && $payment->device_id === $id && $payment->name === 'Money') {
+                        $resultStacker[$id] += $payment->data;
+                    }
+                }
+            }
+        } else {
+            if ($payments === null) {
+                return array(
+                    'profit' => 0,
+                    'stacker' => 0
+                );
+            }
+            $resultStacker = 0;
+            $resultProfit = 0;
+            $lastEncashment = Encashment::getLastEncashmentForDevice($deviceId);
+            $time = $lastEncashment === null ? '' : $lastEncashment->time;
+            $payments = self::find()->andWhere(['device_id' => $deviceId])->andWhere(['between', 'time', $time, date('Y-m-d H:i:s')])->all();
+            foreach ($payments as $payment) {
+                $resultProfit += $payment->data;
+                if ($payment->name === 'Money') {
+                    $resultStacker += $payment->data;
+                }
+            }
+        }
+        return array(
+            'profit' => $resultProfit,
+            'stacker' => $resultStacker
+        );
     }
 
 }
