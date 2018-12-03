15932ce2c38578302ebd9fa4243db9b7fcc63139
vladislav <staik4@yandex.ru>
2018-12-03 17:43:39 +0300

    Add url to buttons/owner

--- views/owner/index.php
+++ views/owner/index.php
@@ -22,6 +22,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Url;
 
 $this->title = 'Устройства';
 $this->params['breadcrumbs'][] = $this->title;
@@ -177,11 +178,12 @@ $this->params['breadcrumbs'][] = $this->title;
                        </div>
 
                        <div class="elem-card-inf buttons d-flex flex-column justify-content-around align-items-center">
+                           <?php $url_statistics = Url::to(['owner/view', 'id' => $id]); $url_cog = Url::to(['owner/config', 'id' => $id]); $url_encashment = Url::to(['owner/encashment', 'id' => $id]);?>
                            <div class="btn-set btn-settings">
-                               <a href=""><span class="oi oi-cog"></span></a>
+                               <a href="<?= $url_cog ?>"><span class="oi oi-cog"></span></a>
                            </div>
                            <div class="btn-set btn-statistics">
-                               <a href=""> <span class="oi oi-document"></span></a>
+                               <a href="<?= $url_statistics ?>"> <span class="oi oi-document"></span></a>
                            </div>
                            <div class="btn-set btn-encashment">
                                <a href="<?= $url_encashment ?>"><span class="oi oi-briefcase"></span></a>
